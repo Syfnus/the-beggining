d6ecec321e9fb310df790bacb42149153242eb8c
Tiago Costa <syfnus@gmail.com>
2021-01-12 20:44:18 -0300

    Projetos e aulas
    Projetos e aulas de diversas linguagens de programação

---
+++ javascript/meu_script.js
@@ -0,0 +1,7 @@
+alert('Olá, seja bem vindo ao curso!!!')
+
+
+/* Selecionar um elemento no DOM (árvore de elementos)
+e atualizar o valor (value) desse elemento com uma string ='') */
+
+document.getElementById('nome').value = 'Oi'
